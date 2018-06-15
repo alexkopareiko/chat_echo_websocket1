ac257d3e3cc4ce5fca3d658003644351b3fad0d5
Alexandr Kopareiko <sanyakopar@gmail.com>
2018-06-15 09:36:19 +0300

    init project

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+export const select = (contact) => {
+
+  return{
+    type: "CONTACT_SELECTED",
+    payload: contact
+  }
+};
+  
---
+++ src/config/dev.js
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+  apiKey: "AIzaSyAkAtZiO50TVwhEXqIxx046kLjAMWopisQ",
+  authDomain: "test-f48e9.firebaseapp.com",
+  databaseURL: "https://test-f48e9.firebaseio.com",
+  projectId: "test-f48e9",
+  storageBucket: "test-f48e9.appspot.com",
+  messagingSenderId: "632389051826"
+};
